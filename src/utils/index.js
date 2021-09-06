130ca0b7ab2deeb4fd369dff2f232dd34eed2f58
rosindubh <p.welsby2@ntlworld.com>
2021-09-06 14:03:53 +0100

    added an if statement to check if there is an actor and a year

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,27 @@
 
 
 exports.goodOrBad = (likeVar, movie, actor, year) => {
-  if (year) {
+  if (actor && year) {
     if (likeVar === "good") {
       console.clear();
       console.log(`I think ${movie} is great movie and ${actor} plays part well.`);
       console.log(`This film was made in the year ${year}`)
     } else if (likeVar=== "bad") {
         console.clear();
-        console.log(`I think ${movie} made in the year ${year } is bad and ${actor} can't act for toffee`);
+        console.log(`I think ${movie} made in the year ${year } is bad and ${actor} can't act for toffee.`);
     } else {
       console.log("Incorrect Input")
     }
   }
   else {
-    console.log('You must enter a year.')
+    console.clear();
+    console.log('You must enter both an actor and a year.');
+    console.log(`You entered the following:`);
+    console.log();
+    console.log(`Did you like it: ${likeVar}`);
+    console.log(`Movie: ${movie}`);
+    console.log(`Actor: ${actor}`);
+    console.log(`Year: ${year}`);
   }
   }
 
