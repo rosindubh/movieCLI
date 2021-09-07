28def14b0729038a827eb825981353277aa1e5eb
rosindubh <p.welsby2@ntlworld.com>
2021-09-07 13:56:53 +0100

    added listDb function to display contents of the database

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,27 +7,46 @@ const addMovie = (likeVar, movie, actor, year) => {
   fs.writeFileSync("./storage.json", stringList)
 }
 
-exports.goodOrBad = (likeVar, movie, actor, year) => {
-  if (likeVar && movie && actor && year) {
-    if (likeVar.toLowerCase() === "good") {
-      addMovie(likeVar, movie, actor, year)
-      console.clear();
-      console.log(`I think ${movie} is a great movie and ${actor} plays part well.`);
-      console.log(`This film was made in the year ${year}`)
-    } else if (likeVar.toLowerCase()=== "bad") {
-        console.clear();
-        console.log(`I think ${movie} made in the year ${year } is bad and ${actor} can't act for toffee.`);
-    } else {
-      console.log(`Incorrect Input`)
+const listDb = (item) => {
+  console.log('Current Db contents...')
+  const list = JSON.parse(fs.readFileSync("./storage.json"));
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i])
+  }
+}
 
-    }
+
+
+exports.goodOrBad = (likeVar, movie, actor, year) => {
+
+
+  if (likeVar.toLowerCase() === "show") {
+    listDb()
   }
   else {
-    var myArgs = process.argv.slice(2);
-    console.clear();
-    console.log('Not enough variables...');
-    console.log(`You entered the following:`);
-    console.log(`\n[${myArgs}]\n`)
+    if (likeVar && movie && actor && year) {
+      if (likeVar.toLowerCase() === "good") {
+        addMovie(likeVar, movie, actor, year)
+        console.clear();
+        console.log(`I think ${movie} is a great movie and ${actor} plays part well.`);
+        console.log(`This film was made in the year ${year}`)
+  
+      } else if (likeVar.toLowerCase()=== "bad") {
+          addMovie(likeVar, movie, actor, year)
+          console.clear();
+          console.log(`I think ${movie} made in the year ${year } is bad and ${actor} can't act for toffee.`);
+      } else {
+        console.log(`Incorrect Input`)
+  
+      }
+    }
+    else {
+      var myArgs = process.argv.slice(2);
+      console.clear();
+      console.log('Not enough variables...');
+      console.log(`You entered the following:`);
+      console.log(`\n[${myArgs}]\n`)
+    }
   }
 }
 
